ece8b95fd538a1f3db8bdaa7ed01d3f21ef9aad4
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-02-23 13:08:20 -0300

    Tweaked values

--- src/game/char/state/Attack.gd
+++ src/game/char/state/Attack.gd
@@ -1,8 +1,9 @@
 extends State
 
 const MOVE_SPEED = 50
-const ATTACK_ANGLE = deg2rad(45)
+const ATTACK_ANGLE = deg2rad(40)
 const ATTACK_STRENGTH = 200
+const ATTACK_MAX_MULTIPLIER = 4 # times the base strength
 
 
 func process(delta: float):
@@ -17,7 +18,7 @@ func _on_AttackArea2D_body_entered(body):
 	if not active():
 		return
 	
-	var strength_modifier = 1 + owner.attack_strength
+	var strength_modifier = 1 + (owner.attack_strength * ATTACK_MAX_MULTIPLIER)
 	var horizontal_modifier = 1 if owner.opposite_side else -1
 	
 	var strength = ATTACK_STRENGTH * strength_modifier
