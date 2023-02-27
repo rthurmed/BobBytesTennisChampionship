42d4cdffcdff65ad18d6ff3592a297b210ddcf33
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-02-27 02:07:27 -0300

    Implemented points system

--- src/Main.gd
+++ src/Main.gd
@@ -1,9 +1,32 @@
 extends Node2D
+class_name Stage
 
+
+signal scored_point(is_by_char1)
+signal gameover(is_char1_winner)
 
 const CAMERA_SHAKE_REFERENCE_POINT = 600
+const BALL_RECOVERY_TIME = 2 # seconds
+
+export var ball_scene: PackedScene
+export var counting_ball_kicks = true
+export var ball_kicks_to_fail = 2
+export var points_to_win = 5
 
 onready var camera_shake = $Camera2D/Shake
+onready var ball_start_position_char1 = $Layers/Positions/BallStart/Char1
+onready var ball_start_position_char2 = $Layers/Positions/BallStart/Char2
+onready var game_layer = $Layers/Game
+
+var ball: Ball
+var ball_on_char1 = true
+var ball_kicks = 0
+var points_char1 = 0
+var points_char2 = 0
+
+
+func _ready():
+	spawn_new_ball(ball_on_char1)
 
 
 func apply_camera_shake(strength):
@@ -13,4 +36,77 @@ func apply_camera_shake(strength):
 
 func _on_Person_attack_executed(strength):
 	apply_camera_shake(strength)
-	$Sounds/HitBall.play()
+	$Sounds/HitRacket.play()
+
+
+func _on_Ball_touched_floor():
+	if not counting_ball_kicks:
+		return
+	
+	ball_kicks = ball_kicks + 1
+	$DebugNode/KicksLabel.text = 'kicks: ' + str(ball_kicks)
+	
+	if ball_kicks >= ball_kicks_to_fail:
+		emit_signal("scored_point", not ball_on_char1)
+		return
+	
+	$Sounds/HitFloor.play()
+
+
+func _on_FieldTransitionArea_body_exited(body):
+	if not body.is_in_group('ball'):
+		return
+	
+	ball_on_char1 = not ball_on_char1
+	ball_kicks = 0
+	
+	$DebugNode/FocusedLabel.text = 'focused: ' + str('char1' if ball_on_char1 else 'char2')
+	$DebugNode/KicksLabel.text = 'kicks: ' + str(ball_kicks)
+
+
+func _on_Main_scored_point(is_by_char1):
+	## disable stage state
+	#counting_ball_kicks = false
+	#ball.set_state(false)
+	
+	# update scores
+	if is_by_char1:
+		points_char1 += 1
+	else:
+		points_char2 += 1
+	
+	$DebugNode/PointsChar1Label.text = 'points char1: ' + str(points_char1)
+	$DebugNode/PointsChar2Label.text = 'points char2: ' + str(points_char2)
+	
+	# is the last point?
+	if (
+		points_char1 >= points_to_win or
+		points_char2 >= points_to_win
+	):
+		emit_signal("gameover", is_by_char1)
+		return
+	
+	# fancy sounds
+	$Sounds/Score.play()
+	
+	# reset ball
+	# TODO: maybe wait a little bit
+	ball.queue_free()
+	ball_kicks = 0
+	ball_on_char1 = is_by_char1
+	spawn_new_ball(is_by_char1)
+	
+	$DebugNode/FocusedLabel.text = 'focused: ' + str('char1' if ball_on_char1 else 'char2')
+	$DebugNode/KicksLabel.text = 'kicks: ' + str(ball_kicks)
+
+
+func spawn_new_ball(to_char1):
+	var target_position = ball_start_position_char1.global_position
+	if not to_char1:
+		target_position = ball_start_position_char2.global_position
+	
+	ball = ball_scene.instance()
+	ball.global_position = target_position
+	var _ok = ball.connect("touched_floor", self, "_on_Ball_touched_floor")
+	
+	game_layer.call_deferred("add_child", ball)
---
+++ src/game/Ball.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+class_name Ball
+
+
+const COLLISION_ENABLED = 4
+const COLLISION_DISABLED = 1
+
+signal touched_floor
+
+
+func set_state(enabled):
+	collision_layer = COLLISION_ENABLED if enabled else COLLISION_DISABLED
+
+
+func _on_Ball_body_entered(body):
+	if body.is_in_group('floor'):
+		emit_signal("touched_floor")
