e3c82ad86aa352768907eb6db2237df08177a1b9
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-03-02 14:40:18 -0300

    Added title screen on gamemode selection

--- src/GamemodeSelect.gd
+++ src/GamemodeSelect.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 onready var first_button = $CanvasLayer/Menu/NormalMode
+onready var animation = $AnimationPlayer
 
 var gamemode_paths = {
 	Enums.Gamemode.NORMAL: "res://src/gamemode/Tennis.tscn",
@@ -10,10 +11,19 @@ var gamemode_paths = {
 	Enums.Gamemode.TRAINING: ""
 }
 var selected_gamemode_path = gamemode_paths[Enums.Gamemode.NORMAL]
+var waiting_starting_input = false
 
 
 func _ready():
-	first_button.grab_focus()
+	waiting_starting_input = false
+	animation.play("start")
+
+
+func _unhandled_input(event):
+	if not waiting_starting_input: return
+	if event is InputEventMouseMotion: return
+	waiting_starting_input = false
+	animation.play("showSelection")
 
 
 func start_exit_sequence(gamemode):
@@ -43,4 +53,17 @@ func _on_BottleTennis_button_up():
 
 
 func _on_TrainingMode_button_up():
+	start_exit_sequence(Enums.Gamemode.TRAINING)
+
+
+func _on_OptionsButton_button_up():
 	pass # Replace with function body.
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "start":
+		waiting_starting_input = true
+	
+	if anim_name == "showSelection":
+		waiting_starting_input = false
+		first_button.grab_focus()
