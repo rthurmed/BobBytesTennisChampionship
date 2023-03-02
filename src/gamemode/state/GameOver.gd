fae6a37a969d380ad412d591431115d68e01227c
Arthur Medeiros <49236663+rthurmed@users.noreply.github.com>
2023-03-01 22:59:13 -0300

    Changed font. Implemented gameover screen

--- src/gamemode/state/GameOver.gd
+++ src/gamemode/state/GameOver.gd
@@ -1,14 +1,32 @@
 extends State
 
 
-func handle_input(_event: InputEvent): pass
-func process(_delta: float): pass
-func physics_process(_delta: float): pass
+onready var animation = $AnimationPlayer
+onready var gameover_title = $Overlay/Gameover/Title
+
+var showed_message = false
+
+
+func process(_delta: float):
+	if not showed_message: return
+	if Input.is_action_just_released("reset"):
+		var _ok = get_tree().reload_current_scene()
 
 
 func enter():
-	# TODO
-	print_debug('Not Implemented')
+	showed_message = false
+	
+	owner.ball.set_collision(false)
+	
+	var char1_won = owner.points_char1 > owner.points_char2
+	var winner = "PLAYER 1" if char1_won else "PLAYER 2"
+	
+	gameover_title.text = str(winner, " WON!")
+	
+	animation.play("gameover")
 
 
-func exit(): pass
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if not active(): return
+	if not anim_name == "gameover": return
+	showed_message = true
